3acc81cd2ef9648c171b4a99dd14eb0d709eb17b
tnewnhamcode <148156667+tnewnhamcode@users.noreply.github.com>
2025-11-21 11:22:53 +0000

    Update settings.py

--- eventbooker/settings.py
+++ eventbooker/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0kzrez7syk_+ibnmjv==x-la&cxnegs@)(x@rnod*@rww@z&+$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
