f4aac64266e9d9c2ecaf44264daef6f5af90e857
tnewnhamcode <148156667+tnewnhamcode@users.noreply.github.com>
2025-11-21 11:28:17 +0000

    Update settings.py

--- eventbooker/settings.py
+++ eventbooker/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-0kzrez7syk_+ibnmjv==x-la&cxnegs@)(x@rnod*@rww@z&+$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["tom-n-event-booker1.onrender.com"]
 
 
 # Application definition
